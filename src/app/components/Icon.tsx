e61aaba3140578d58dcb040bef03f23e11435b7a
Simon Reinisch <simon@reinisch.io>
2020-04-21 15:18:57 +0200

    Add Icon-component and centralize icon management

---
+++ src/app/components/Icon.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import {h}    from 'preact';
+import {cn}   from '../../utils/preact-utils';
+import styles from './Icon.module.scss';
+
+// Load icons dynamically
+const icons = new Map();
+const svgContext = require.context('../../assets/icons', false, /\.svg$/);
+for (const path of svgContext.keys()) {
+    const nameWithExt = path.slice(2);
+    const name = nameWithExt.slice(0, -4);
+    icons.set(name, require(`../../assets/icons/${nameWithExt}`));
+}
+
+type Props = {
+    name: string;
+    className?: string;
+};
+
+export default ({name, className = ''}: Props) => {
+    const svg = icons.get(name);
+
+    if (!svg) {
+        throw new Error(`Icon not found: ${name}`);
+    }
+
+    return (
+        <div dangerouslySetInnerHTML={{__html: svg}}
+             className={cn(className, styles.icon)}/>
+    );
+};
--- src/app/tabs/uploads/statusIcon.tsx
+++ src/app/tabs/uploads/statusIcon.tsx
@@ -1,47 +1,24 @@
 import {h}           from 'preact';
 import {JSXInternal} from 'preact/src/jsx';
 import {UploadState} from '../../../state/models/Uploads';
+import Icon          from '../../components/Icon';
 
 export const getStatusIconFor = (status: UploadState): JSXInternal.Element => {
     switch (status) {
         case 'idle':
         case 'paused':
-            return (
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
-                    <path d="M 10 5.25 L 10 44.746094 L 43.570313 25 Z"/>
-                </svg>
-            );
+            return <Icon name="play"/>;
         case 'running':
-            return (
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
-                    <path d="M 12 8 L 12 42 L 22 42 L 22 8 Z M 28 8 L 28 42 L 38 42 L 38 8 Z"/>
-                </svg>
-            );
+            return <Icon name="pause"/>;
         case 'removed':
         case 'cancelled':
         case 'errored':
-        case 'timeout': {
-            return (
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-                    <path
-                        d="M 12.355469 3 L 12.785156 19 L 17.199219 19 L 17.644531 3 L 12.355469 3 z M 15.007812 22 C 13.364813 22 12.339844 22.953813 12.339844 24.507812 C 12.339844 26.047813 13.365813 27 15.007812 27 C 16.649812 27 17.662109 26.047812 17.662109 24.507812 C 17.662109 22.953813 16.650813 22 15.007812 22 z"/>
-                </svg>
-            );
-        }
+        case 'timeout':
+            return <Icon name="exclamation-mark"/>;
         case 'finished':
-            return (
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path
-                        d="M 6 6 C 4.3550302 6 3 7.3550302 3 9 L 3 15 C 3 16.64497 4.3550302 18 6 18 L 8 18 C 9.6449698 18 11 16.64497 11 15 L 11 9 C 11 7.3550302 9.6449698 6 8 6 L 6 6 z M 13 6 L 13 18 L 15 18 L 15 13.332031 L 18.501953 18 L 21 18 L 16.5 12 L 21 6 L 18.501953 6 L 15 10.667969 L 15 6 L 13 6 z M 6 8 L 8 8 C 8.5650302 8 9 8.4349698 9 9 L 9 15 C 9 15.56503 8.5650302 16 8 16 L 6 16 C 5.4349698 16 5 15.56503 5 15 L 5 9 C 5 8.4349698 5.4349698 8 6 8 z"/>
-                </svg>
-            );
+            return <Icon name="ok"/>;
         case 'peer-cancelled': {
-            return (
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-                    <path
-                        d="M 15.089844 3 C 10.131844 3 7.3202344 5.7778281 7.2402344 9.7988281 L 7.2402344 10 L 11.617188 10 L 11.617188 9.7988281 C 11.713187 8.0228281 12.941828 6.8769531 14.798828 6.8769531 C 16.639828 6.8769531 17.867188 7.9413594 17.867188 9.4433594 C 17.867188 10.945359 17.237297 11.720844 15.154297 12.964844 C 12.925297 14.272844 12.036094 15.726547 12.246094 18.310547 L 12.271484 19 L 16.558594 19 L 16.558594 18.34375 C 16.558594 16.79375 17.156688 16.000812 19.304688 14.757812 C 21.581688 13.416813 22.759766 11.721828 22.759766 9.2988281 C 22.759766 5.5678281 19.708844 3 15.089844 3 z M 14.507812 22 C 12.864813 22 11.839844 22.953813 11.839844 24.507812 C 11.839844 26.047813 12.865813 27 14.507812 27 C 16.149812 27 17.162109 26.047812 17.162109 24.507812 C 17.162109 22.953813 16.150813 22 14.507812 22 z"/>
-                </svg>
-            );
+            return <Icon name="question-mark"/>;
         }
     }
 };
