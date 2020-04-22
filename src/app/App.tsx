c299e2adba6f92ec8bd3bac504f2d7b0689a6b83
Simon Reinisch <simon@reinisch.io>
2020-04-22 20:44:36 +0200

    Add update-screen

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,16 @@
-import {Component, h} from 'preact';
-import styles         from './App.module.scss';
+import {Component, h}  from 'preact';
+import styles          from './App.module.scss';
 import {DropZone}     from './overlays/DropZone';
+import {UpdateScreen} from './overlays/UpdateScreen';
 import {Toast}        from './overlays/Toast';
-import {Tabs}         from './tabs/Tabs';
+import {Tabs}          from './tabs/Tabs';
 
 export class App extends Component {
     render() {
         return (
             <div className={styles.app}>
                 {Toast.getElement()}
+                <UpdateScreen/>
                 <Tabs/>
                 <DropZone/>
             </div>
