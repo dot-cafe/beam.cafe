2dcd63a6e026226ff0accbc572d2479bfc6558b3
Simon Reinisch <simon@reinisch.io>
2020-05-06 20:04:22 +0200

    Make TabView a functional component; Enhance mobile tab-view-transition

--- src/app/tabs/container/TabViews.tsx
+++ src/app/tabs/container/TabViews.tsx
@@ -1,28 +1,24 @@
-import {Component, h} from 'preact';
-import {JSXInternal}  from 'preact/src/jsx';
-import {cn}           from '../../../utils/preact-utils';
-import styles         from './TabViews.module.scss';
+import {FunctionalComponent, h} from 'preact';
+import {JSXInternal}            from 'preact/src/jsx';
+import {cn}                     from '../../../utils/preact-utils';
+import styles                   from './TabViews.module.scss';
 
 type Props = {
     views: Array<JSXInternal.Element>;
     activeView: number;
 };
 
-type State = {};
-
-export class TabViews extends Component<Props, State> {
-
-    render() {
-        const {views, activeView} = this.props;
-
-        return (
-            <div className={styles.tabViews}>
-                {views.map((com, i) => (
-                    <div className={cn(styles.view, {
-                        [styles.active]: i === activeView
-                    })} key={i}>{com}</div>
-                ))}
-            </div>
-        );
-    }
-}
+export const TabViews: FunctionalComponent<Props> = (
+    {views, activeView}
+) => (
+    <div className={styles.tabViews}
+         data-tab-index={activeView}>
+        <div>
+            {views.map((com, i) => (
+                <div className={cn(styles.view, {
+                    [styles.active]: i === activeView
+                })} key={i}>{com}</div>
+            ))}
+        </div>
+    </div>
+);
