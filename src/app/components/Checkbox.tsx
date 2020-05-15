c269ba97105ca71ff36904428d9004b2e64fcfba
Simon Reinisch <simon@reinisch.io>
2020-05-15 23:30:22 +0200

    Make Checkbox and ContextMenu a functional component

--- src/app/components/Checkbox.tsx
+++ src/app/components/Checkbox.tsx
@@ -1,32 +1,20 @@
-import {Component, h} from 'preact';
-import {bind, cn}     from '../../utils/preact-utils';
-import styles         from './Checkbox.module.scss';
+import {FunctionalComponent, h} from 'preact';
+import {cn}                     from '../../utils/preact-utils';
+import styles                   from './Checkbox.module.scss';
 
 type Props = {
     checked: boolean;
     onChange: (state: boolean) => void;
 };
 
-type State = {};
-
-export class Checkbox extends Component<Props, State> {
-
-    @bind
-    click() {
-        this.props.onChange(!this.props.checked);
-    }
-
-    render({checked}: Props) {
-        return (
-            <button onClick={this.click}
-                    className={cn(styles.checkbox, {
-                        [styles.checked]: checked
-                    })}>
-                <svg xmlns="http://www.w3.org/2000/svg"
-                     viewBox="0 0 50 50">
-                    <path d="M7,25L17.571,38,44,12"/>
-                </svg>
-            </button>
-        );
-    }
-}
+export const Checkbox: FunctionalComponent<Props> = ({checked, onChange}) => (
+    <button onClick={() => onChange(!checked)}
+            className={cn(styles.checkbox, {
+                [styles.checked]: checked
+            })}>
+        <svg xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 50 50">
+            <path d="M7,25L17.571,38,44,12"/>
+        </svg>
+    </button>
+);
--- src/app/components/ContextMenu.tsx
+++ src/app/components/ContextMenu.tsx
@@ -1,8 +1,8 @@
-import {Component, h} from 'preact';
-import {JSXInternal}  from 'preact/src/jsx';
-import {cn}           from '../../utils/preact-utils';
-import styles         from './ContextMenu.module.scss';
-import {Popper}       from './Popper';
+import {FunctionalComponent, h} from 'preact';
+import {JSXInternal}            from 'preact/src/jsx';
+import {cn}                     from '../../utils/preact-utils';
+import styles                   from './ContextMenu.module.scss';
+import {Popper}                 from './Popper';
 
 export type ContextMenuButtons = Array<{
     id: string;
@@ -17,35 +17,24 @@ type Props = {
     button: JSXInternal.Element;
 };
 
-type State = {
-    open: boolean;
-};
-
-export class ContextMenu extends Component<Props, State> {
-    action(id: string) {
-        return () => this.props.onAction(id);
+export const ContextMenu: FunctionalComponent<Props> = (
+    {
+        button,
+        content,
+        onAction,
+        className = ''
     }
-
-    render() {
-        const {
-            button,
-            content,
-            className = ''
-        } = this.props;
-
-        return (
-            <Popper className={cn(styles.contextMenu, className, {
-                [styles.open]: this.state.open
-            })} button={button} content={
+) => (
+    <Popper className={cn(styles.contextMenu, className)}
+            button={button}
+            content={
                 <div className={styles.buttonList}>{
                     content.map((value, index) => (
-                        <button key={index} onClick={this.action(value.id)}>
+                        <button key={index} onClick={() => onAction(value.id)}>
                             {value.icon ? <bc-icon name={value.icon}/> : ''}
                             <span>{value.text}</span>
                         </button>
                     ))
                 }</div>
             }/>
-        );
-    }
-}
+);
