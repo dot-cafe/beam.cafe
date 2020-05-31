2a620768a6398e6d0dbc8a1f941bf9e8f06e6920
Simon Reinisch <simon@reinisch.io>
2020-05-31 13:15:52 +0200

    Auto-focus checkbox if checked
    Fix focus-color in UploadStreamItem

--- src/app/components/Checkbox.tsx
+++ src/app/components/Checkbox.tsx
@@ -1,6 +1,7 @@
-import {cn}                     from '@utils/preact-utils';
-import {FunctionalComponent, h} from 'preact';
-import styles                   from './Checkbox.module.scss';
+import {cn}                                from '@utils/preact-utils';
+import {createRef, FunctionalComponent, h} from 'preact';
+import {useEffect}                         from 'preact/hooks';
+import styles                              from './Checkbox.module.scss';
 
 type Props = {
     'aria-label'?: string;
@@ -16,16 +17,30 @@ export const Checkbox: FunctionalComponent<Props> = (
         className = '',
         ...props
     }
-) => (
-    <button role="checkbox"
-            aria-label={props['aria-label']}
-            onClick={(ev: MouseEvent) => onChange(!checked, ev)}
-            className={cn(styles.checkbox, className, {
-                [styles.checked]: checked
-            })}>
-        <svg xmlns="http://www.w3.org/2000/svg"
-             viewBox="0 0 50 50">
-            <path d="M7,25L17.571,38,44,12"/>
-        </svg>
-    </button>
-);
+) => {
+    const btn = createRef<HTMLButtonElement>();
+    let lastState = checked;
+
+    useEffect(() => {
+        if (checked && lastState) {
+            btn.current?.focus();
+        }
+
+        lastState = checked;
+    });
+
+    return (
+        <button role="checkbox"
+                ref={btn}
+                aria-label={props['aria-label']}
+                onClick={(ev: MouseEvent) => onChange(!checked, ev)}
+                className={cn(styles.checkbox, className, {
+                    [styles.checked]: checked
+                })}>
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 50 50">
+                <path d="M7,25L17.571,38,44,12"/>
+            </svg>
+        </button>
+    );
+};
